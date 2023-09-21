7c6d7423f4626229dbe34ed1e7979d3903cefe1a
fulajtarova <114149698+fulajtarova@users.noreply.github.com>
2023-09-21 12:06:08 +0200

    fix

--- start.py
+++ start.py
@@ -6,9 +6,8 @@ def up(board):
     for i in range(len(board)):
         for j in range(len(board[i])):
             if board[i][j] == 0:
-                if i > 0:
-                    board[i][j], board[i - 1][j] = board[i - 1][j], board[i][j]
-                return board
+                board[i][j], board[i - 1][j] = board[i - 1][j], board[i][j]
+            return board
 
 
 # function to move the blank tile down
@@ -16,9 +15,8 @@ def down(board):
     for i in range(len(board)):
         for j in range(len(board[i])):
             if board[i][j] == 0:
-                if i < len(board) - 1:
-                    board[i][j], board[i + 1][j] = board[i + 1][j], board[i][j]
-                return board
+                board[i][j], board[i + 1][j] = board[i + 1][j], board[i][j]
+            return board
 
 
 # function to move the blank tile left
@@ -26,9 +24,8 @@ def left(board):
     for i in range(len(board)):
         for j in range(len(board[i])):
             if board[i][j] == 0:
-                if j > 0:
-                    board[i][j], board[i][j - 1] = board[i][j - 1], board[i][j]
-                return board
+                board[i][j], board[i][j - 1] = board[i][j - 1], board[i][j]
+            return board
 
 
 # function to move the blank tile to the right
@@ -36,9 +33,8 @@ def right(board):
     for i in range(len(board)):
         for j in range(len(board[i])):
             if board[i][j] == 0:
-                if j < len(board[i]) - 1:
-                    board[i][j], board[i][j + 1] = board[i][j + 1], board[i][j]
-                return board
+                board[i][j], board[i][j + 1] = board[i][j + 1], board[i][j]
+            return board
 
 
 # function to print the board
@@ -94,25 +90,58 @@ def is_solved(board, board_end):
     return True
 
 
+# function to pu board in a 1d list
+def flatten_board(board):
+    return [num for row in board for num in row if num != 0]
+
+
+# function to count inversions
+def count_inversions(arr):
+    inversions = 0
+    for i in range(len(arr)):
+        for j in range(i + 1, len(arr)):
+            if arr[i] > arr[j]:
+                inversions += 1
+    return inversions
+
+
+# function to check if the board is solvable
+def is_solvable(board_start, board_end):
+    flat_start = flatten_board(board_start)
+    flat_end = flatten_board(board_end)
+
+    inversions_start = count_inversions(flat_start)
+    inversions_end = count_inversions(flat_end)
+
+    return inversions_start % 2 == inversions_end % 2
+
+
 # main function to test the code
 def start():
-    # main_board = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
-    # board = random_board(board)
-    # board_end = random_board(board)
-    board = [[5, 7, 2], [3, 8, 1], [6, 0, 4]]
-    board_end = [[5, 7, 2], [3, 8, 1], [6, 0, 4]]
+    # unsovable board
+    # board = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+    # board_end = [[2, 1, 3], [4, 5, 6], [7, 8, 0]]
 
-    # board_end = [[6, 0, 3], [2, 5, 1], [7, 8, 4]]
+    main_board = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+
+    board = random_board(main_board)
+    board_end = random_board(main_board)
+
+    print("Start:")
     print_board(board)
-    # print_board(board_end)
+    print("End:")
+    print_board(board_end)
 
-    print(wrong_tiles(board, board_end))
-    tiles_distance(board, board_end)
+    print("Is solvable: ")
+    print(is_solvable(board, board_end))
 
-    board = right(board)
-    print_board(board)
-
+    print("Is solved: ")
     print(is_solved(board, board_end))
+
+    print("Wrong tiles: ")
+    print(wrong_tiles(board, board_end))
+    print("Tiles distance: ")
+    print(tiles_distance(board, board_end))
 
 
 start()
