bffa059d8b6daa16387f518ac3c88901f122fa10
fulajtarova <114149698+fulajtarova@users.noreply.github.com>
2023-09-22 10:55:45 +0200

    a

--- start.py
+++ start.py
@@ -93,96 +93,6 @@ def tiles_distance(board, goal):
     return distance
 
 
-# Function to create the A* search tree
-def a_star(node, board_end, heuristic_threshold=float("inf")):
-    empty_tile_index = node.board.index(0)
-    # has a left child
-    if empty_tile_index % 3 > 0:
-        left_board = left(node.board)
-        left_heuristic = calculate_heuristic(left_board, board_end)
-        if left_heuristic <= heuristic_threshold:
-            node.left = Node(left_board)
-            node.left.heuristic = left_heuristic
-            node.left.depth = node.depth + 1
-    # has a right child
-    if empty_tile_index % 3 < 2:
-        right_board = right(node.board)
-        right_heuristic = calculate_heuristic(right_board, board_end)
-        if right_heuristic <= heuristic_threshold:
-            node.right = Node(right_board)
-            node.right.heuristic = right_heuristic
-            node.right.depth = node.depth + 1
-    # has an up child
-    if empty_tile_index >= 3:
-        up_board = up(node.board)
-        up_heuristic = calculate_heuristic(up_board, board_end)
-        if up_heuristic <= heuristic_threshold:
-            node.up = Node(up_board)
-            node.up.heuristic = up_heuristic
-            node.up.depth = node.depth + 1
-    # has a down child
-    if empty_tile_index < 6:
-        down_board = down(node.board)
-        down_heuristic = calculate_heuristic(down_board, board_end)
-        if down_heuristic <= heuristic_threshold:
-            node.down = Node(down_board)
-            node.down.heuristic = down_heuristic
-            node.down.depth = node.depth + 1
-
-    return node
-
-
-"""
-# Function to create the A* search tree
-def a_star_recursive(board_start, board_end):
-    start_node = Node(board_start)
-    start_node.heuristic = calculate_heuristic(start_node.board, board_end)
-
-    open_list = [start_node]
-    closed_set = set()
-
-    while open_list:
-        current_node = min(open_list, key=lambda node: node.depth + node.heuristic)
-        open_list.remove(current_node)
-
-        if current_node.board == board_end:
-            return current_node
-
-        closed_set.add(tuple(current_node.board))
-
-        empty_tile_index = current_node.board.index(0)
-        possible_moves = []
-
-        if empty_tile_index % 3 > 0:
-            possible_moves.append(("left", -1))
-        if empty_tile_index % 3 < 2:
-            possible_moves.append(("right", 1))
-        if empty_tile_index >= 3:
-            possible_moves.append(("up", -3))
-        if empty_tile_index < 6:
-            possible_moves.append(("down", 3))
-
-        for move, move_offset in possible_moves:
-            new_board = current_node.board[:]
-            new_board[empty_tile_index], new_board[empty_tile_index + move_offset] = (
-                new_board[empty_tile_index + move_offset],
-                new_board[empty_tile_index],
-            )
-            new_node = Node(
-                new_board,
-                operation=move,
-                depth=current_node.depth + 1,
-                parent=current_node,
-            )
-            new_node.heuristic = calculate_heuristic(new_node.board, board_end)
-
-            if tuple(new_node.board) not in closed_set:
-                open_list.append(new_node)
-
-    return None
-"""
-
-
 class Node:
     def __init__(
         self,
@@ -207,6 +117,19 @@ class Node:
 
     def __str__(self):
         return f"Board: {self.board},Operand: {self.operand}, Depth: {self.depth}, F: {self.f}"
+
+    def print_solution(self):
+        path = []
+        current_node = self
+
+        while current_node:
+            if current_node.operand:
+                path.append(current_node.operand)
+            current_node = current_node.parent
+
+        path.reverse()
+        print("Solution path:")
+        print(" -> ".join(path))
 
 
 # Function to calculate the heuristic value (wrong tiles)
@@ -251,11 +174,20 @@ def insert(node, board_end, max_depth=10):
     min_value = min([item[1] for item in comparison], default=float("inf"))
     print(min_value)
 
-    for item in comparison:
-        if item[1] == min_value:
-            child = Node(board=item[0], operand=None, depth=node.depth + 1, parent=node)
-            node.l_child = insert(child, board_end, max_depth)
-            break
+    for i in range(len(comparison)):
+        if comparison[i] and comparison[i][1] == min_value:
+            move_operands = ["left", "right", "up", "down"]
+            child_operand = move_operands[i]
+
+            child = Node(
+                board=comparison[i][0],
+                operand=child_operand,
+                depth=node.depth + 1,
+                parent=node,
+            )
+
+            # Set the correct child attribute based on the operand
+            setattr(node, f"{child_operand}_child", insert(child, board_end, max_depth))
 
     return node
 
@@ -271,7 +203,7 @@ def start():
     # board_end = [1, 2, 3, 4, 5, 6, 7, 8, 0]
 
     board = [1, 2, 3, 4, 5, 6, 7, 8, 0]
-    board_end = [1, 2, 3, 4, 5, 6, 7, 0, 8]
+    board_end = [1, 2, 3, 4, 5, 6, 0, 7, 8]
 
     # board = random_board()
     # board_end = random_board()
@@ -281,36 +213,13 @@ def start():
     print("End:")
     print_board(board_end)
 
-    """node = Node(board)
-
-    while is_solved(node.board, board_end) == False:
-        node = a_star(node, board_end)"""
-
     root = Node(board)
     root.f = root.depth + calculate_heuristic(root.board, board_end)
     print(root)
 
     root = insert(root, board_end, 10)
 
-    """
-    solution_node = a_star_recursive(board, board_end)
-
-    if solution_node:
-        print("Solution path:")
-        path = []
-        current_node = solution_node
-        while current_node:
-            path.append(current_node)
-            current_node = current_node.parent
-        path.reverse()
-        for node in path:
-            print_board(node.board)
-            print("Operation:", node.operation)
-            print("Depth:", node.depth)
-            print("Heuristic:", node.heuristic)
-            print()
-    else:
-        print("No solution found.")"""
+    print(root)
 
 
 start()
