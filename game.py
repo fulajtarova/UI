2e91fffa340ba1be6ec7263f17c62200db5b59a9
fulajtarova <114149698+fulajtarova@users.noreply.github.com>
2023-10-05 15:46:46 +0200

    done

--- game.py
+++ game.py
@@ -3,8 +3,12 @@ import random
 import copy
 
 solution = None
+num_of_moves_h1 = 0
+num_of_moves_h2 = 0
 
 """
+------------------------------------------------------------------------------------------------------------------------
+Methods to rotate the board 
 ------------------------------------------------------------------------------------------------------------------------
 """
 
@@ -55,13 +59,15 @@ def right(board, n):
 
 """
 ------------------------------------------------------------------------------------------------------------------------
+Methods to print the board
+------------------------------------------------------------------------------------------------------------------------
 """
 
 
 # function to print the board
-def print_board(board, m, n):
+def print_board(board, n):
     new_board = ""
-    for i in range(m):
+    for i in range(n):
         for j in range(n):
             new_board += f"{board[i*n+j]:2d} "
         new_board += "\n"
@@ -71,18 +77,22 @@ def print_board(board, m, n):
 
 """
 ------------------------------------------------------------------------------------------------------------------------
+Method to generate a random board
+------------------------------------------------------------------------------------------------------------------------
 """
 
 
 # function to generate a random board
-def random_board(m, n):
-    numbers = list(range(m * n))
+def random_board(n):
+    numbers = list(range(n * n))
     random.shuffle(numbers)
     board = [numbers]
     return board[0]
 
 
 """
+------------------------------------------------------------------------------------------------------------------------
+Method to check if the board is solved
 ------------------------------------------------------------------------------------------------------------------------
 """
 
@@ -93,6 +103,8 @@ def is_same(board, board_end):
 
 
 """
+------------------------------------------------------------------------------------------------------------------------
+Methods to calculate the heuristic values for the board configurations 
 ------------------------------------------------------------------------------------------------------------------------
 """
 
@@ -130,6 +142,8 @@ def heuristic_2_tiles_distance(board, goal):
 
 """
 ------------------------------------------------------------------------------------------------------------------------
+Node class that contains the board configuration, the move that led to this board, the depth of this node and the f value
+------------------------------------------------------------------------------------------------------------------------
 """
 
 
@@ -139,10 +153,6 @@ class Node:
         board=[],
         operand=None,
         depth=0,
-        up_child=None,
-        down_child=None,
-        right_child=None,
-        left_child=None,
         parent=None,
         f=None,
     ):
@@ -150,14 +160,7 @@ class Node:
         self.operand = operand  # The move that led to this board (e.g., "up", "down", "right", "left")
         self.depth = depth  # The depth of this node in the tree
         self.f = f
-        self.up_child = up_child  # Up child
-        self.down_child = down_child  # Down child
-        self.right_child = right_child  # Right child
-        self.left_child = left_child  # Left child
         self.parent = parent
-
-    def __str__(self):
-        return f"Board: {self.board},Operand: {self.operand}, Depth: {self.depth}, F: {self.f}"
 
     # Define a custom comparison method for nodes
     def __lt__(self, other):
@@ -166,27 +169,32 @@ class Node:
 
 """
 ------------------------------------------------------------------------------------------------------------------------
+Method to calculate the heuristic value based on the choice 
+------------------------------------------------------------------------------------------------------------------------
 """
 
 
-def calculate_heuristic(board, board_end):
-    return heuristic_1_wrong_tiles(board, board_end)
+def calculate_heuristic(choice, board, board_end):
+    if choice == 1:
+        return heuristic_1_wrong_tiles(board, board_end)
+    elif choice == 2:
+        return heuristic_2_tiles_distance(board, board_end)
 
 
 """
 ------------------------------------------------------------------------------------------------------------------------
+A* Algorithm function with methods to insert child nodes into the open list
+------------------------------------------------------------------------------------------------------------------------
 """
-
-# Initialize a set for the closed list
-closed_set = set()
 
 
 # Function to insert child nodes into the open list
-def insert(node, board_end, open_list, n, max_depth=50):
-    if is_same(node.board, board_end) or node.depth >= max_depth:
+def insert(node, board_end, open_list, n, choice):
+    # Check if the current node's board configuration is the same as the goal board configuration
+    # If it is, then we have found the solution and we can stop the search
+    if is_same(node.board, board_end):
         global solution
         solution = copy.deepcopy(node)
-        print("Solution found!")
         return None
 
     # Check if the node's board configuration is already in the closed set
@@ -196,52 +204,64 @@ def insert(node, board_end, open_list, n, max_depth=50):
     # Add the current board configuration to the closed set
     closed_set.add(tuple(node.board))
 
+    # Generate possible child nodes by moving in different directions (left, right, up, down)
     l_ch = left(node.board, n)
     r_ch = right(node.board, n)
     u_ch = up(node.board, n)
     d_ch = down(node.board, n)
 
+    # Check if moving left is a valid move and not the same as the parent
     if not is_same(l_ch, node.board):
+        # Create a child node with the new board configuration, depth, parent and f value
         child = Node(
             l_ch,
             "left",
             node.depth + 1,
             parent=node,
-            f=node.depth + 1 + calculate_heuristic(l_ch, board_end),
+            f=node.depth + 1 + calculate_heuristic(choice, l_ch, board_end),
         )
+        # Check if the child's board configuration is not in the open list
         if child.board not in open_list:
             heapq.heappush(open_list, (child.f, child))
 
+    # Check if moving right is a valid move and not the same as the parent
     if not is_same(r_ch, node.board):
+        # Create a child node with the new board configuration, depth, parent and f value
         child = Node(
             r_ch,
             "right",
             node.depth + 1,
             parent=node,
-            f=node.depth + 1 + calculate_heuristic(r_ch, board_end),
+            f=node.depth + 1 + calculate_heuristic(choice, r_ch, board_end),
         )
+        # Check if the child's board configuration is not in the open list
         if child.board not in open_list:
             heapq.heappush(open_list, (child.f, child))
 
+    # Check if moving up is a valid move and not the same as the parent
     if not is_same(u_ch, node.board):
+        # Create a child node with the new board configuration, depth, parent and f value
         child = Node(
             u_ch,
             "up",
             node.depth + 1,
             parent=node,
-            f=node.depth + 1 + calculate_heuristic(u_ch, board_end),
+            f=node.depth + 1 + calculate_heuristic(choice, u_ch, board_end),
         )
+        # Check if the child's board configuration is not in the open list
         if child.board not in open_list:
             heapq.heappush(open_list, (child.f, child))
-
+    # Check if moving down is a valid move and not the same as the parent
     if not is_same(d_ch, node.board):
+        # Create a child node with the new board configuration, depth, parent and f value
         child = Node(
             d_ch,
             "down",
             node.depth + 1,
             parent=node,
-            f=node.depth + 1 + calculate_heuristic(d_ch, board_end),
+            f=node.depth + 1 + calculate_heuristic(choice, d_ch, board_end),
         )
+        # Check if the child's board configuration is not in the open list
         if child.board not in open_list:
             heapq.heappush(open_list, (child.f, child))
 
@@ -249,35 +269,100 @@ def insert(node, board_end, open_list, n, max_depth=50):
 
 
 # Start of A* Algorithm
-def astar(board_start, board_end, n):
-    global solution
-    start_node = Node(board_start)
+def astar(board_start, board_end, n, choice):
+    # Reset the closed set for each new A* search
+    global closed_set, solution, num_of_moves_h1, num_of_moves_h2
+    closed_set = set()
     solution = None
+
+    # Initialize the starting node with the given board configuration
+    start_node = Node(board_start)
+
+    # Create an empty priority queue (open list)
     open_list = []
+
+    # Initialize the node count for limiting and stopping the search if it takes too long
+    node_count = 0
+
+    # Push the starting node onto the open list with its estimated cost (f)
     heapq.heappush(open_list, (start_node.f, start_node))
+
+    # Main A* search loop
     while open_list:
+        # Pop the node with the lowest estimated cost (f) from the open list
         _, current_node = heapq.heappop(open_list)
-        insert(current_node, board_end, open_list, n, 50)
+
+        # Increment the node count
+        node_count += 1
+
+        # Limit the search to 50000 nodes and stop if it takes too long
+        if node_count >= 50000:
+            print("Reached 50000 nodes, exiting...")
+            if choice == 1:
+                num_of_moves_h1 = 0
+            elif choice == 2:
+                num_of_moves_h2 = 0
+            break
+
+        # Insert child nodes into the open list and check for the solution
+        insert(current_node, board_end, open_list, n, choice)
+
+        # If a solution is found, break out of the loop
         if solution:
             break
 
+    # If a solution is found, print the solution path
     if solution:
-        print_solution_path(solution)
+        print_solution_path(solution, choice)
 
 
-# Rest of the code remains the same
+"""
+------------------------------------------------------------------------------------------------------------------------
+Method to print the solution path
+------------------------------------------------------------------------------------------------------------------------
+"""
 
 
 # Function to print the solution path
-def print_solution_path(solution_node):
+def print_solution_path(solution_node, choice):
+    i = 0
     path = []
     current_node = solution_node
+    # Traverse the tree from the solution node to the root node
+    # and append the moves with board configuration to the path list in reverse order
     while current_node:
         if current_node.operand:
-            path.append(current_node.operand)
+            i += 1
+            move = current_node.operand
+            board = current_node.board
+            path.append((move, board))
         current_node = current_node.parent
 
     path.reverse()
+
+    # Print the solution path in the correct order with the move number, move and board configuration
     print("Solution Path:")
-    for move in path:
-        print(move)
+    for i in range(len(path)):
+        print(f"Move: {i + 1:<8} {path[i][0]:<10} {path[i][1]}")
+
+    # Update the number of moves for the heuristic choice
+    global num_of_moves_h1, num_of_moves_h2
+
+    if choice == 1:
+        num_of_moves_h1 = i + 1
+    elif choice == 2:
+        num_of_moves_h2 = i + 1
+
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+Method to get the number of moves for the heuristic choice for comparison
+------------------------------------------------------------------------------------------------------------------------
+"""
+
+
+def get_num_of_moves(choice):
+    if choice == 1:
+        return num_of_moves_h1
+    elif choice == 2:
+        return num_of_moves_h2
--- main.py
+++ main.py
@@ -1,27 +1,62 @@
+import time
 import game
-from termcolor import colored
+
+# colors
+Red = "\033[0;31m"
+Green = "\033[0;32m"
+Orange = "\033[0;33m"
+Blue = "\033[0;34m"
+Purple = "\033[0;35m"
+Cyan = "\033[0;36m"
+White = "\033[0;37m"
+black = "\033[0;30m"
+black = "\033[0;90m"
+red = "\033[0;91m"
+green = "\033[0;92m"
+yellow = "\033[0;93m"
+blue = "\033[0;94m"
+magenta = "\033[0;95m"
+cyan = "\033[0;96m"
+white = "\033[0;97m"
+cyan_back = "\033[0;46m"
+pink_back = "\033[0;45m"
+white_back = "\033[0;47m"
+blue_back = "\033[0;44m"
+orange_back = "\033[0;43m"
+green_back = "\033[0;42m"
+red_back = "\033[0;41m"
+grey_back = "\033[0;40m"
+bold = "\033[1m"
+underline = "\033[4m"
+italic = "\033[3m"
+darken = "\033[2m"
+reset = "\033[0m"
 
 
-def is_valid_board(board, m, n):
+def is_valid_board(board, n):
     if not all(isinstance(x, int) for x in board):
         return False
 
-    if len(board) != m * n:
+    if len(board) != n * n:
         return False
 
     if len(set(board)) != len(board):
         return False
 
-    if not all(0 <= x < m * n for x in board):
+    if not all(0 <= x < n * n for x in board):
         return False
 
     return True
 
 
 def main():
-    print(colored("Welcome to the puzzle solver.", "green"))
+    print(pink_back + "Welcome to the puzzle solver." + reset)
     while True:
-        print("\nDo you want to enter the board or generate a random board?")
+        print(
+            green
+            + "\nDo you want to enter the board or generate a random board?"
+            + reset
+        )
         print("Enter 'q' to quit.")
         print("1. Enter the board")
         print("2. Generate a random board")
@@ -29,58 +64,140 @@ def main():
         choice = input("\nEnter your choice: ")
 
         if choice == "q":
+            print("\nThank you for playing! Leaving the game...")
             break
         elif choice == "1":
-            print("Please enter the size of the board!\n")
-            m = int(input("Enter the number of rows: "))
-            n = int(input("Enter the number of columns: "))
+            n = int(input("Enter the number of rows and columns: "))
 
             while True:
                 try:
                     board_start = [
                         int(x)
                         for x in input(
-                            f"\nEnter the starting board from 0 to {m*n-1}: "
-                        )
+                            f"\nEnter the starting board (space-separated) from 0 to {n*n-1}: "
+                        ).split()
                     ]
                     board_end = [
                         int(x)
-                        for x in input(f"Enter the ending board from 0 to {m*n-1}: ")
+                        for x in input(
+                            f"Enter the ending board (space-separated) from 0 to {n*n-1}: "
+                        ).split()
                     ]
 
-                    if is_valid_board(board_start, m, n) and is_valid_board(
-                        board_end, m, n
-                    ):
+                    if is_valid_board(board_start, n) and is_valid_board(board_end, n):
                         break
                     else:
                         print("\nInvalid input! Please enter the board again!")
 
                 except ValueError:
                     print(
-                        "\nInvalid input! Please enter valid integers without spaces."
+                        "\nInvalid input! Please enter valid integers separated by spaces."
                     )
 
-            print("Valid input:")
-            print(f"Starting board:\n{game.print_board(board_start, m, n)}")
-            print(f"\nEnding board:\n{game.print_board(board_end, m, n)}")
+            print(cyan + "\nBoards:" + reset)
+            print(f"Starting board:\n{game.print_board(board_start, n)}")
+            print(f"\nEnding board:\n{game.print_board(board_end, n)}")
         elif choice == "2":
-            print("Please enter the size of the board!\n")
-            m = int(input("Enter the number of rows: "))
             n = int(input("Enter the number of columns: "))
 
-            board_start = game.random_board(m, n)
-            board_end = game.random_board(m, n)
-            print("\nRandomly generated boards:")
-            print(f"Starting board:\n{game.print_board(board_start, m, n)}")
-            print(f"\nEnding board:\n{game.print_board(board_end, m, n)}")
+            board_start = game.random_board(n)
+            board_end = game.random_board(n)
+            print(cyan + "\nRandomly generated boards:" + reset)
+            print(f"Starting board:\n{game.print_board(board_start, n)}")
+            print(f"\nEnding board:\n{game.print_board(board_end,  n)}")
         else:
             print(
                 "\nInvalid choice. Please enter '1' to enter the board or '2' to generate a random board."
             )
 
-        print(colored("\nSolving the puzzle...\n", "blue"))
-        game.astar(board_start, board_end, n)
+        board_start1 = board_start.copy()
+        board_start2 = board_start.copy()
+
+        print(blue + "\nSolving the puzzle with 1. heuristic-wrong tiles...\n" + reset)
+        t10 = time.time()
+        game.astar(board_start1, board_end, n, 1)
+        t11 = time.time()
+        t1f = t11 - t10
+
+        print(
+            blue
+            + "\nSolving the puzzle with 2. heuristic-manhattan distance...\n"
+            + reset
+        )
+
+        t20 = time.time()
+        game.astar(board_start2, board_end, n, 2)
+        t21 = time.time()
+        t2f = t21 - t20
+
+        if game.get_num_of_moves(1) != 0 and game.get_num_of_moves(2) != 0:
+            print(magenta + "\n\nComparing the two heuristics:" + reset)
+
+            print(f"\nTime taken for heuristic-wrong tiles: {round(t1f*1000,3)} ms")
+            print(
+                f"Time taken for heuristic-manhattan distance: {round(t2f*1000,3)} ms"
+            )
+
+            print(
+                f"\nNumber of steps for heuristic-wrong tiles: {game.get_num_of_moves(1)}"
+            )
+            print(
+                f"Number of steps for heuristic-manhattan distance: {game.get_num_of_moves(2)}",
+            )
+
+            if t1f < t2f:
+                print(
+                    f"\nHeuristic-wrong tiles is faster than heuristic-manhattan distance"
+                )
+            elif t1f > t2f:
+                print(
+                    f"\nHeuristic-manhattan distance is faster than heuristic-wrong tiles"
+                )
+            else:
+                print(
+                    f"\nHeuristic-wrong tiles and heuristic-manhattan distance take the same time"
+                )
+
+            if game.get_num_of_moves(1) < game.get_num_of_moves(2):
+                print(
+                    f"Heuristic-wrong tiles takes {game.get_num_of_moves(2) - game.get_num_of_moves(1)} more steps than heuristic-manhattan distance"
+                )
+            elif game.get_num_of_moves(1) > game.get_num_of_moves(2):
+                print(
+                    f"Heuristic-manhattan distance takes {game.get_num_of_moves(1) - game.get_num_of_moves(2)} more steps than heuristic-wrong tiles"
+                )
+            else:
+                print(
+                    f"Heuristic-wrong tiles and heuristic-manhattan distance take the same number of steps"
+                )
+        elif game.get_num_of_moves(1) == 0 and game.get_num_of_moves(2) != 0:
+            print(magenta + "\n\nComparing the two heuristics:" + reset)
+            print("\nHeuristic-wrong tiles was not able to find a solution")
+            print(
+                f"\nTime taken for heuristic-manhattan distance: {round(t2f*1000,3)} ms"
+            )
+            print(
+                f"Number of steps for heuristic-manhattan distance: {game.get_num_of_moves(2)}",
+            )
+        elif game.get_num_of_moves(2) == 0 and game.get_num_of_moves(1) != 0:
+            print(magenta + "\n\nComparing the two heuristics:" + reset)
+            print("\nHeuristic-manhattan distance was not able to find a solution")
+            print(f"\nTime taken for heuristic-wrong tiles: {round(t1f*1000,3)} ms")
+            print(
+                f"Number of steps for heuristic-wrong tiles: {game.get_num_of_moves(1)}"
+            )
+
+        else:
+            print(Orange + "\nNo solution found in either case" + reset)
 
 
 if __name__ == "__main__":
     main()
+
+# 123045678
+# 012345678
+
+# 123804765
+
+# 567408321 start
+# 123804765 end
